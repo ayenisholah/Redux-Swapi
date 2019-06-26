6f7220d97b5305f35156d9fafabe9bec581f29b1
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-26 15:58:59 +0100

    get character function created

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore,  applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from "./reducers";
+// import { charsReducer } from './reducers/starWarsReducer';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
+
 
 const store = createStore(
   rootReducer,
