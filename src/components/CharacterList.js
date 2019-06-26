c06937749a41fcd4909d10ddfdbe8031fc6ea710
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-26 15:59:18 +0100

    Characterlist refactor

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,11 +4,7 @@ import Character from "./Character";
 
 const CharacterList = props => {
   return (
-    <ul>
-      {props.characters.map(character => {
-        return <Character key={character.name} character={character} />;
-      })}
-    </ul>
+    <></>
   );
 };
 
